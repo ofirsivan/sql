93803c0a0faeadcbebb9e0025bfaa74c9bf8e56e
Ofir <ofir.sivan@mail.utoronto.ca>
2025-11-08 04:04:44 -0500

    attached is the first assigment

--- 02_activities/assignments/DC_Cohort/assignment1.sql
+++ 02_activities/assignments/DC_Cohort/assignment1.sql
@@ -5,49 +5,126 @@
 --SELECT
 /* 1. Write a query that returns everything in the customer table. */
 
-
+SELECT * 
+FROM customer;
 
 /* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table, 
 sorted by customer_last_name, then customer_first_ name. */
 
+SELECT*
+FROM customer
+ORDER BY customer_first_name, customer_last_name 
+LIMIT 10; 
 
 
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
 
+SELECT *
+FROM customer_purchases
+WHERE product_id = 4
+OR product_id = 9;
 
-
-/*2. Write a query that returns all customer purchases and a new calculated column 'price' (quantity * cost_to_customer_per_qty), 
+/*2. Write a query that returns all customer purchases and a new calculated column 'price' (quantity * cost_to_customer_per_qty),
 filtered by customer IDs between 8 and 10 (inclusive) using either:
 	1.  two conditions using AND
 	2.  one condition using BETWEEN
 */
 -- option 1
 
+SELECT 
+quantity, 
+cost_to_customer_per_qty,
+quantity * cost_to_customer_per_qty as price, 
+product_id, 
+market_date,
+vendor_id, 
+customer_id, 
+transaction_time
+
+FROM customer_purchases  
+
+WHERE customer_id > 7
+AND customer_id < 11 ;
 
 -- option 2
 
+SELECT *
+FROM customer_purchases
+WHERE product_id = 4
+OR product_id = 9;
 
+/*2. Write a query that returns all customer purchases and a new calculated column 'price' (quantity * cost_to_customer_per_qty),
+filtered by customer IDs between 8 and 10 (inclusive) using either:
+	1.  two conditions using AND
+	2.  one condition using BETWEEN
+*/
+-- option 1
+
+SELECT 
+quantity, 
+cost_to_customer_per_qty,
+quantity * cost_to_customer_per_qty as price, 
+product_id, 
+market_date,
+vendor_id, 
+customer_id, 
+transaction_time
+
+FROM customer_purchases  
+
+WHERE customer_id BETWEEN 8 AND 10;
 
 --CASE
 /* 1. Products can be sold by the individual unit or by bulk measures like lbs. or oz. 
 Using the product table, write a query that outputs the product_id and product_name
 columns and add a column called prod_qty_type_condensed that displays the word “unit” 
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
+SELECT 
+product_id, 
+product_name, 
+product_qty_type
 
+,CASE 
+	WHEN product_qty_type = 'unit' THEN 'unit'
+	ELSE 'bulk'
+END as prod_qty_type_condensed
 
+FROM product;
 
 /* 2. We want to flag all of the different types of pepper products that are sold at the market. 
 add a column to the previous query called pepper_flag that outputs a 1 if the product_name 
 contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
 
+SELECT 
+product_id, 
+product_name, 
+product_qty_type
 
+,CASE 
+	WHEN product_qty_type = 'unit' THEN 'unit'
+	ELSE 'bulk'
+END as prod_qty_type_condensed
+
+,CASE 
+	WHEN product_name LIKE '%eppers%' THEN 1
+	ELSE 0
+END as pepper_flag
+
+FROM product;
 
 --JOIN
 /* 1. Write a query that INNER JOINs the vendor table to the vendor_booth_assignments table on the 
 vendor_id field they both have in common, and sorts the result by vendor_name, then market_date. */
 
+SELECT *
 
+FROM vendor as v 	
+INNER JOIN vendor_booth_assignments as vba
+	ON v.vendor_id = vba.vendor_id
+
+	
+ORDER BY vendor_name, market_date; 
 
 
 /* SECTION 3 */
@@ -56,14 +133,30 @@ vendor_id field they both have in common, and sorts the result by vendor_name, t
 /* 1. Write a query that determines how many times each vendor has rented a booth 
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
 
-
+ SELECT vendor_id ,COUNT (market_date) as number_booth_renting
+ FROM vendor_booth_assignments
+ GROUP BY vendor_id; 
 
 /* 2. The Farmer’s Market Customer Appreciation Committee wants to give a bumper 
 sticker to everyone who has ever spent more than $2000 at the market. Write a query that generates a list 
 of customers for them to give stickers to, sorted by last name, then first name. 
 
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
+SELECT 
+cp. customer_id, 
+customer_first_name,
+customer_last_name,
+SUM (quantity*cost_to_customer_per_qty) as total_spend
 
+FROM customer_purchases as cp
+INNER JOIN customer as c
+	ON c.customer_id = cp.customer_id
+	
+GROUP BY c.customer_id
+
+HAVING total_spend > 2000
+
+ORDER BY customer_last_name, customer_first_name; 
 
 
 --Temp Table
@@ -78,9 +171,23 @@ When inserting the new vendor, you need to appropriately align the columns to be
 VALUES(col1,col2,col3,col4,col5) 
 */
 
+DROP TABLE IF EXISTS temp.new_vendor;
+
+CREATE TABLE temp.new_vendor AS
+
+SELECT *
+
+FROM vendor; 
+
+INSERT INTO temp.new_vendor (vendor_id, vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name )
+VALUES(10,'Thomass Superfood Store', 'Fresh Focused store', 'Thomas', 'Rosenthal')  
 
 
--- Date
+
+
+
+
+-- Date [no need for this assigment]
 /*1. Get the customer_id, month, and year (in separate columns) of every purchase in the customer_purchases table.
 
 HINT: you might need to search for strfrtime modifers sqlite on the web to know what the modifers for month 
